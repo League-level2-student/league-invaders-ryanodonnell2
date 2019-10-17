80e095b7966f22a4a455d07f39158b6e225c4e69
SMHS-519-09 <SMHS-519-09@SMHS-519-09.smhsrop.lab>
2019-10-17 15:23:38 -0700

    Part 1 done

--- src/LeagueInvaders.java
+++ src/LeagueInvaders.java
@@ -1,4 +1,24 @@
+import java.awt.Dimension;
+
+import javax.swing.JFrame;
 
 public class LeagueInvaders {
-
+	JFrame frame;
+	final int width = 500;
+	final int height = 800;
+	
+	public static void main(String[] args) {
+		new LeagueInvaders().setup();
+	}
+	
+	LeagueInvaders(){
+		 frame = new JFrame();
+	}
+	void setup() {
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setVisible(true);
+		frame.setSize(new Dimension(width, height));
+		frame.pack();
+	}
+	
 }
